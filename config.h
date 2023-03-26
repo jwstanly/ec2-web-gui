3bfd533112d5a6293b724c7c8625f43d86afd62f
willway09 <willway@outlook.com>
2023-03-25 20:15:39 -0400

    Launch management Chromium instance

--- config.h
+++ config.h
@@ -49,22 +49,26 @@ static const Layout layouts[] = {
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
+#define AltMask Mod1Mask
+#define WindowsMask Mod4Mask
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "xterm", NULL };
+static const char *nullcmd[]  = {  NULL };
 static const char *chromiumcmd[]  = { "chromium", NULL };
+static const char *launchchromiumcmd[]  = { "launch_chromium", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ AltMask | ShiftMask, XK_Return, spawn,          {.v = nullcmd } },
 };
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkRootWin,        0,              Button1,        spawn,          {.v = chromiumcmd } },
+	{ ClkRootWin,        0,              Button1,        spawn,          {.v = launchchromiumcmd } },
+	{ ClkRootWin,        AltMask | ShiftMask,              Button1,        spawn,          {.v = chromiumcmd } },
 };
-
